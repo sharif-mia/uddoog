47961a4d1f3d40c0e31a71f4cbbddec5032a3400
SHARIF-HP\Sharif Mia <sharif.mia@ssd-tech.io>
2025-06-22 21:16:14 +0600

    my products filter added

--- src/views/myProducts/MyProducts.vue
+++ src/views/myProducts/MyProducts.vue
@@ -1,20 +1,38 @@
 <template>
 
-  <h2 class="page__title col-gap d-flex">
+  <h2 class="page__title col-gap">
     My Products
-    <div class="d-flex gap-3 ml-auto">
-      <router-link class="btn" :to="{ name: 'addProducts' }">
+    
+  </h2>
+  <div class="d-flex mb-4 align-items-center">
+      <div class="d">
+        <router-link class="btn" :to="{ name: 'addProducts' }">
         <i class="icon-plus"></i>
         <span>Add New Products</span>
       </router-link>
-      <div class="sort-filter">
+      </div>
+      <div class="ml-auto d-flex align-items-center gap-3">
+        <div class="nav__search"><button class="nav__search__btn"><i class="icon icon-search"></i></button><input class="form-control" type="text" placeholder="What are you looking for"></div>
+      <el-select
+      v-model="category"
+      placeholder="Select Category"
+      size="large"
+      style="width: 240px"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+        <div class="sort-filter">
         <label class="sort-filter__option">Sort by</label>
         <button class="sort-filter__option sort-filter__option--active">Orders</button>
         <button class="sort-filter__option">Price</button>
       </div>
+      </div>
     </div>
-  </h2>
-  
   <div v-if="products.length > 0" class="product-grid">
     <Product v-for="product in products" :key="product.id" :showWishlist="false" :showCart="true" :showStore="false" :product="product" />
     <router-link class="upload" :to="{ name: 'addProducts' }">
@@ -48,6 +66,29 @@ export default {
       products: [],
       loadingProducts: true,
       errorProducts: null,
+      category:"",
+      options : [
+  {
+    value: 'Category 1',
+    label: 'Category1',
+  },
+  {
+    value: 'Category 2',
+    label: 'Category2',
+  },
+  {
+    value: 'Category 3',
+    label: 'Category3',
+  },
+  {
+    value: 'Category 4',
+    label: 'Category4',
+  },
+  {
+    value: 'Category 5',
+    label: 'Category5',
+  },
+]
     };
   },
   computed: {},
